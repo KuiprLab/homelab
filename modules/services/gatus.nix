0425f99f83facca446c55940f28981ec68444292
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-09-14 11:56:54 +0200

    chore: automatic commit before deployment

--- modules/services/gatus.nix
+++ modules/services/gatus.nix
@@ -225,6 +225,15 @@
     caddyVirtualHosts."gatus.int.kuipr.de" = {
       extraConfig = ''
         reverse_proxy localhost:8888
+
+        forward_auth authelia:9091 {
+                uri /api/authz/forward-auth
+                ## The following commented line is for configuring the Authelia URL in the proxy. We strongly suggest
+                ## this is configured in the Session Cookies section of the Authelia configuration.
+                # uri /api/authz/forward-auth?authelia_url=https://auth.example.com/
+                copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+        }
+
       '';
       name = "Gatus";
     };
